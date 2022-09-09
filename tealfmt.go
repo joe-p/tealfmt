3eef2d4ebb5813934d8340c1a782331d59d73830
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-09 10:25:50 -0400

    comments to code and newline after commented ops, skip empty lines

--- tealfmt.go
+++ tealfmt.go
@@ -11,41 +11,6 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-type Config struct {
-	File    string `docopt:"<file>"`
-	InPlace bool   `docopt:"-i,--inplace"`
-}
-type Line struct {
-	Text     string
-	Comments []string
-	IsVoid   bool
-	IsLabel  bool
-	Op       string
-}
-
-// ToString returns a string representing the TEAL line
-// With any comments above it matching its indent settings
-func (l *Line) ToString(insideBody bool) string {
-	indent := ""
-	if insideBody {
-		indent = "\t"
-	}
-
-	newline := ""
-	if l.IsVoid || l.IsLabel {
-		newline = "\n"
-	}
-
-	comments := ""
-	for _, comment := range l.Comments {
-		comments += fmt.Sprintf("%s%s\n", indent, comment)
-	}
-
-	line := fmt.Sprintf("%s%s%s", indent, l.Text, newline)
-
-	return fmt.Sprintf("%s%s\n", comments, line)
-}
-
 const (
 	version = "tealfmt v0.1.0"
 	usage   = `tealfmt
@@ -75,6 +40,46 @@ var (
 	voidOpMatch  = regexp.MustCompile("^(" + strings.Join(voidOps, "|") + ")$")
 )
 
+type Config struct {
+	File    string `docopt:"<file>"`
+	InPlace bool   `docopt:"-i,--inplace"`
+}
+type Line struct {
+	Text     string
+	Comments []string
+	IsVoid   bool
+	IsLabel  bool
+	Op       string
+}
+
+// ToString returns a string representing the TEAL line
+// With any comments above it matching its indent settings
+func (l *Line) ToString(insideBody bool) string {
+	// If we're inside the body of a label
+	// we should add a tab
+	indent := ""
+	if insideBody {
+		indent = "\t"
+	}
+
+	// A new line is added after a label, void op, or any op with comments
+	newline := ""
+	if l.IsVoid || l.IsLabel || len(l.Comments) > 0 {
+		newline = "\n"
+	}
+
+	// Set appropriate indent on each comment
+	comments := ""
+	for _, comment := range l.Comments {
+		comments += fmt.Sprintf("%s%s\n", indent, comment)
+	}
+
+	// Add any indent, and an extra newline if necessary
+	line := fmt.Sprintf("%s%s%s", indent, l.Text, newline)
+
+	return fmt.Sprintf("%s%s\n", comments, line)
+}
+
 func main() {
 	opts, err := docopt.ParseArgs(usage, os.Args[1:], version)
 	if err != nil {
@@ -82,8 +87,7 @@ func main() {
 	}
 
 	config := Config{}
-	err = opts.Bind(&config)
-	if err != nil {
+	if err = opts.Bind(&config); err != nil {
 		log.Fatalf("failed to bind args: %s:", err)
 	}
 
@@ -93,13 +97,18 @@ func main() {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+	var (
+		newLines    []Line
+		commentBuff []string
 
-	// First pass, add spacing
-	newLines := []Line{}
-	commentBuff := []string{}
+		scanner = bufio.NewScanner(file)
+	)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" {
+			continue
+		}
 
 		// If its the pragma line, add it without modification
 		if versionMatch.MatchString(line) {
@@ -107,10 +116,14 @@ func main() {
 			continue
 		}
 
+		// If its a comment, add it to the buffer to be
+		// associated with the first non comment line
 		if commentRegex.MatchString(line) {
 			commentBuff = append(commentBuff, line)
 			continue
 		}
+
+		// Construct a Line from the comment buf and flags
 		op := opMatch.FindString(line)
 		newLines = append(newLines, Line{
 			Text:     line,
@@ -120,26 +133,29 @@ func main() {
 			IsLabel:  labelRegex.MatchString(line),
 		})
 
+		// Reset the comment buff
 		commentBuff = nil
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to scan file: %+v", err)
 	}
 
 	output := ""
-
 	insideBody := false
 	for idx, line := range newLines {
 		if idx > 0 && newLines[idx-1].IsLabel {
 			insideBody = true
 		}
 
-		indented := insideBody && !line.IsLabel
-
+		// Remove newline from a previous isVoid
 		if idx > 0 && line.IsVoid && newLines[idx-1].IsVoid {
 			output = output[:len(output)-1]
 		}
+
+		// We should indent if we're inside a body and
+		// haven't hit a new label
+		indented := insideBody && !line.IsLabel
 
 		output += line.ToString(indented)
 	}
